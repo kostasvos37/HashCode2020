5c24d93d1b56ed486fe6d84fe75a52f86ec6f0f8
KostasVos <el16435@central.ntua.gr>
2020-02-20 21:11:50 +0200

    Read files

---
+++ hashCode.py
@@ -0,0 +1,67 @@
+class Library:
+    def __init__(self, N, T, M, ids):
+        self.books = N
+        self.signup = T
+        self.rate = M
+        self.bookIDs = [int(i) for i in ids]
+    
+    def printDetails(self):
+        print("Books : ", self.books, "Signup time : ", self.signup, "Rate : ", self.rate)
+        print("Book ids : ")
+        for i in self.bookIDs:
+            print(i, end=" ")
+        print("")
+
+
+class Solver:    
+    def __init__(self, B, L, D, scores):
+        self.days = D
+        self.books = B
+        self.libraries = L
+        self.bookScores = [int(i) for i in scores]
+        self.libraryList = []
+
+    def printScores(self):
+        print("Books : ", self.books, "Libraries : ", self.libraries, "Days : ", self.days)
+        print("Book Scores : ")
+        for i in self.bookScores:
+            print(i, end=" ")
+        print ("")
+    
+    def addLibrary(self, library):
+        self.libraryList.append(library)
+
+    def printLibraryDetails(self):
+        id = 0
+        print("=========== Library Details ============")
+        for i in self.libraryList:
+            print("Library id = " , id)
+            i.printDetails()
+            id = id+1
+        print("")
+
+
+input = "./inputs/a_example.txt"
+inputFile = open(input)
+
+line1 = inputFile.readline()
+arr1 = line1.split(" ")
+arr1 = [int(i) for i in arr1]
+line2 = inputFile.readline()
+scores = line2.split(" ")
+solution = Solver(arr1[0],arr1[1],arr1[2],scores)
+solution.printScores()
+for i in range(arr1[1]):
+    libStats = inputFile.readline()
+    libstats = [int(i) for i in libStats.split(" ")]
+
+    ids = inputFile.readline()
+    books = ids.split(" ")
+
+    newLibrary = Library(libstats[0], libstats[1], libstats[2], books)
+
+    solution.addLibrary(newLibrary)
+solution.printLibraryDetails()
+
+
+
